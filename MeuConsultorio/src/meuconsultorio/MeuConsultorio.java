cee41011ebed80a704cdf82137ddd15ee1b775a7
Guilherme Eco <guilherme_eco@hotmail.com>
2015-08-13 20:42:56 -0300

    Codigo dividido em metodos e reorganizacao dos menus.

--- MeuConsultorio/src/meuconsultorio/MeuConsultorio.java
+++ MeuConsultorio/src/meuconsultorio/MeuConsultorio.java
@@ -10,190 +10,158 @@ import util.Console;
  * @author 631120345
  */
 public class MeuConsultorio {
-//
-//    Paciente cadPaciente = new Paciente();
-//
-//    public MeuConsultorio() {
-//
-//    }
-//
-//    public void menuPrincipal() {
-//
-//        @SuppressWarnings("UnusedAssignment")
-//        int opcao = 0;
-//
-//        do {
-//            System.out.println("--------------------------------");
-//            System.out.println("1 - Cadastro de pacientes.");
-//            System.out.println("2 - Cadastro de medicamentos.");
-//            System.out.println("3 - Agendamento de consultas.");
-//            System.out.println("4 - Registro de consultas.");
-//            System.out.println("5 - Histórico");
-//            System.out.println("6 - Sair");
-//            System.out.println("--------------------------------");
-//            opcao = Console.scanInt("Informe o menu desejado: ");
-//            switch (opcao) {
-//                case 1:
-//                    menuCadPacientes();
-//                    break;
-//                case 2:
-//                    menuCadMedicamentos();
-//                    break;
-//                case 3:
-//                    agendaConsultas();
-//                    break;
-//                case 4:
-//                    cadastraConsultas();
-//                    ;
-//                    break;
-//                case 5:
-//                    historicoDeConsultas();
-//                    break;
-//                case 6:
-//                    System.out.println("Volte sempre!!");
-//                    break;
-//                default:
-//                    System.out.println("Opção incorreta.");
-//            }
-//        } while (opcao != 6);
-//    }
-//
-//    private void menuCadPacientes() {
-//        @SuppressWarnings("UnusedAssignment")
-//        int opcao = 0;
-//        do {
-//            System.out.println("--------------------------------");
-//            System.out.println("1 - Adicionar novo pacientes.");
-//            System.out.println("2 - Visualizar todos os pacientes.");
-//            System.out.println("3 - Sair");
-//            System.out.println("--------------------------------");
-//            opcao = Console.scanInt("Informe o menu desejado: ");
-//            switch (opcao) {
-//                case 1:
-//                    cadPacientes();
-//                    break;
-//                case 2:
-//                    consultaPacientes();
-//                    break;
-//                case 3:
-//                    System.out.println("Retornando ao menu anterior.");
-//                    break;
-//                default:
-//                    System.out.println("Opção incorreta.");
-//            }
-//        } while (opcao != 3);
-//    }
-//
-//    private void menuCadMedicamentos() {
-//        @SuppressWarnings("UnusedAssignment")
-//        int opcao = 0;
-//        do {
-//            System.out.println("--------------------------------");
-//            System.out.println("1 - Adicionar novo medicamento.");
-//            System.out.println("2 - Visualizar todos os medicamentos.");
-//            System.out.println("3 - Sair");
-//            System.out.println("--------------------------------");
-//            opcao = Console.scanInt("Informe o menu desejado: ");
-//            switch (opcao) {
-//                case 1:
-//                    break;
-//                case 2:
-//                    break;
-//                case 3:
-//                    System.out.println("Retornando ao menu anterior.");
-//                    break;
-//                default:
-//                    System.out.println("Opção incorreta.");
-//            }
-//        } while (opcao != 3);
-//    }
-//
-//    private void agendaConsultas() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    private void cadastraConsultas() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    private void historicoDeConsultas() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    private void cadPacientes() {
-//        @SuppressWarnings("UnusedAssignment")
-//        String var1 = null;
-//        var1 = Console.scanString("\nInforme o nome do paciente: ");
-//        cadPaciente.setNome(var1);
-//        var1 = Console.scanString("\nInforme a data de nascimento do paciente: ");
-//        cadPaciente.setDataNascimento(var1);
-//        var1 = Console.scanString("\nInforme o RG do paciente: ");
-//        cadPaciente.setRg(var1);
-//    }
-//
-//    private void consultaPacientes() {
-//        System.out.println("--------------------------------");
-//        System.out.println("Nome: " + cadPaciente.getNome()
-//                + "\nData de Nascimento: " + cadPaciente.getDataNascimento()
-//                + "\nRG: " + cadPaciente.getRg());
-//    }
+
+    private List<Paciente> listaPacientes = new ArrayList<Paciente>();
+
+    public MeuConsultorio() {
+
+        menuPrincipal();
+        int op = -1;
+    }
+
+    private void menuPrincipal() {
+
+        @SuppressWarnings("UnusedAssignment")
+        int op = -1;
+
+        do {
+            System.out.println("\nMenu: ");
+            System.out.println("1 - Cadastro de pacientes.");
+            System.out.println("2 - Cadastro de medicamentos.");
+            System.out.println("3 - Agendamento de consultas.");
+            System.out.println("4 - Registro de consultas.");
+            System.out.println("5 - Histórico");
+            System.out.println("6 - Sair");
+            System.out.println("--------------------------------");
+            try {
+                op = Console.scanInt("Informe o menu desejado: ");
+                switch (op) {
+                    case 1:
+                        menuCadPacientes();
+                        break;
+                    case 2:
+                        menuCadMedicamentos();
+                        break;
+                    case 3:
+                        agendaConsultas();
+                        break;
+                    case 4:
+                        cadastraConsultas();
+                        ;
+                        break;
+                    case 5:
+                        historicoDeConsultas();
+                        break;
+                    case 6:
+                        System.out.println("Volte sempre!!");
+                        break;
+                    default:
+                        System.out.println("Opção incorreta.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Opção errada, não pode informar letras");
+            }
+        } while (op != 6);
+    }
+
+    private void menuCadPacientes() {
+
+        int op = -1;
+        do {
+            System.out.println("--------------------------------");
+            System.out.println("1 - Adicionar novo pacientes.");
+            System.out.println("2 - Visualizar todos os pacientes.");
+            System.out.println("3 - Sair");
+            System.out.println("--------------------------------");
+            try {
+                op = Console.scanInt("Informe o menu desejado: ");
+
+                switch (op) {
+                    case 1:
+                        cadPacientes();
+                        break;
+                    case 2:
+                        consultaPacientes();
+                        break;
+                    case 3:
+                        System.out.println("Retornando ao menu anterior.");
+                        break;
+                    default:
+                        System.out.println("Opção incorreta.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Opção errada, não pode informar letras");
+            }
+        } while (op != 3);
+    }
+
+    private void menuCadMedicamentos() {
+        @SuppressWarnings("UnusedAssignment")
+        int opcao = 0;
+        do {
+            System.out.println("--------------------------------");
+            System.out.println("1 - Adicionar novo medicamento.");
+            System.out.println("2 - Visualizar todos os medicamentos.");
+            System.out.println("3 - Sair");
+            System.out.println("--------------------------------");
+            opcao = Console.scanInt("Informe o menu desejado: ");
+            switch (opcao) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Retornando ao menu anterior.");
+                    break;
+                default:
+                    System.out.println("Opção incorreta.");
+            }
+        } while (opcao != 3);
+    }
+
+    private void agendaConsultas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void cadastraConsultas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void historicoDeConsultas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void cadPacientes() {
+        System.out.println("\nCadatro de paciente");
+        String rg = Console.scanString("RG: ");
+        String nome = Console.scanString("Nome: ");
+        String dataNascimento = Console.scanString("Data de Nascimento: ");
+        Paciente paciente = new Paciente(rg, nome, dataNascimento);
+        listaPacientes.add(paciente);
+    }
+
+    private void consultaPacientes() {
+        System.out.println("\nLista de pacientes");
+        if (listaPacientes.isEmpty()) {
+            System.out.println("<<Lista Vazia>>");
+        } else {
+            System.out.print(String.format("%-10s", "RG"));
+            System.out.print(String.format("%-20s", "NOME"));
+            System.out.println(String.format("%-10s", "NASCIMENTO"));
+            for (Paciente p : listaPacientes) {
+                System.out.print(String.format("%-10s", p.getRg()));
+                System.out.print(String.format("%-20s", p.getNome()));
+                System.out.println(String.format("%-10s", p.getDataNascimento()));
+            }
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        /*
-         MeuConsultorio executa = new MeuConsultorio();
-         executa.menuPrincipal();
-         */
-
-        int op = -1;
-        List<Paciente> listaPacientes = new ArrayList<Paciente>();
-
-        do {
-            System.out.println("Menu: ");
-            System.out.println("1 - Cadastrar Paciente");
-            System.out.println("2 - Listar Pacientes");
-            System.out.println("3 - Sair da Aplicação");
-            try {
-                op = Console.scanInt("Digite a opção do menu: ");
-
-                switch (op) {
-                    case 1:
-                        System.out.println("\nCadatro de paciente");
-                        String rg = Console.scanString("RG: ");
-                        String nome = Console.scanString("Nome: ");
-                        String dataNascimento = Console.scanString("Data de Nascimento: ");
-                        Paciente paciente = new Paciente(rg, nome, dataNascimento);
-                        listaPacientes.add(paciente);
-                        break;
-                    case 2:
-                        System.out.println("\nLista de pacientes");
-                        if (listaPacientes.isEmpty()) {
-                            System.out.println("<<Lista Vazia>>");
-                        } else {
-                            System.out.print(String.format("%-10s", "RG"));
-                            System.out.print(String.format("%-20s", "NOME"));
-                            System.out.println(String.format("%-10s", "NASCIMENTO"));
-                            for (Paciente p : listaPacientes) {
-                                System.out.print(String.format("%-10s", p.getRg()));
-                                System.out.print(String.format("%-20s", p.getNome()));
-                                System.out.println(String.format("%-10s", p.getDataNascimento()));
-                            }
-                        }
-                        break;
-                    case 3:
-                        System.out.println("Saindo da aplicação...");
-                        break;
-                    default:
-                        System.out.println("Opção inválida!");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Opção errada, não pode informar letras");
-            }
-        } while (op != 3);
-
+        MeuConsultorio meuConsultorio = new MeuConsultorio();
     }
 
 }
